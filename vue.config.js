8bbb054c1a98d10913f74d90418f48b2d15279b0
Brandon Lee <brandonlee781@gmail.com>
2020-10-26 12:41:34 -0500

    Undo that change

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/alone-against-the-flames/alone/'
+    ? '/alone-against-the-flames/'
     : '/',
   chainWebpack: (config) => {
     const svgRule = config.module.rule('svg');
