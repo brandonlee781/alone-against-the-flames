d64c6c5b1fae7ac60d32d1061b6a7f08f35a788a
Brandon Lee <brandonlee781@gmail.com>
2020-10-26 12:37:08 -0500

    Changed public path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/alone-against-the-flames/'
+    ? '/alone-against-the-flames/alone/'
     : '/',
   chainWebpack: (config) => {
     const svgRule = config.module.rule('svg');
